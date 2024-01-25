74b1fe7147558eafaac2d37ef31872b389a71fb5
Hayagriva-Parthasarathy <hayagriva02@gmail.com>
2024-01-25 11:25:52 +0530

    1.1

--- grocery_mngmt.py
+++ grocery_mngmt.py
@@ -3,30 +3,43 @@ prod=[]
 tot_price=0
 price=0
 product_found = False
+iteration=1
+already_found = False
 def calc_amt():
     global tot_price
     global product_found
+    global iteration
+    global prod
+    global already_found
     while True:
-        cart=input("Do you want to add to cart: ")
+        if(iteration ==1):
+            cart=input("ADD PRODUCTS TO CART (yes/no):")
+            iteration+=1
+        else:
+           cart = input("ADD ANOTHER PRODUCT TO CART (yes/no):")
         if(cart == "yes"):
-            item= input("Enter the product name: ")
+            item= input("ENTER THE PRODUCT NAME: ")
             for search_item in product.keys():
                 if(search_item == item):
                     product_found = True
             if(product_found):
-                qty=int(input("How many? " ))
+                qty=int(input("HOW MANY? " ))
                 price=product[item]*qty
                 tot_price+=price
+                prod.append(item)
+                prod.append(product[item])
+                prod.append(qty)
+                prod.append(price)
                 product_found=False
             else:
                 while True:
-                    print("Product not available")
-                    item=input("Enter the product name: ")
+                    print("PRODUCT NOT AVAILABLE")
+                    item=input("ENTER THE PRODUCT NAME: ")
                     for search_item in product.keys():
                         if(search_item == item):
                             product_found = True
                     if(product_found):
-                        qty=int(input("How many? " ))
+                        qty=int(input("HOW MANY? " ))
                         price=product[item]*qty
                         tot_price+=price
                         product_found=False
@@ -34,7 +47,13 @@ def calc_amt():
                
         else:
             return tot_price
-           
+        
+def products_list():
+    return prod
+    
+
 final_price = calc_amt()
-print("Final price is: ")
+products = products_list()
+print("PURCHASED PRODUCTS ARE ",products)
+print("FINAL PRICE IS: ")
 print(final_price)
